c3fad75d57f77a8225b4540515cad0ff26296705
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-11-23 19:37:43 -0300

    aula segurança

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,2 +1,15 @@
 module BlogHelper
+
+    def trata_html(html)
+        doc = Nokogiri::HTML(html)
+
+        doc.search('script').each do |script_tag|
+          if script_tag['src']&.include?('jquery') || script_tag['src']&.include?('popper') || script_tag['src']&.include?('bootstrap') 
+            next
+          end
+          script_tag.remove
+        end
+
+        doc.inner_html
+    end
 end
